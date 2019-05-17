ef092dc598c444b4c406980fea7e05567f490de3
Javier Antón Moral <javier.anton@juanxxiii.net>
2019-05-17 12:40:17 +0200

    Agregar archivos de proyecto.

---
+++ DosNumeros/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DosNumeros
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.lbSalida = new System.Windows.Forms.ListBox();
+            this.bValorarNumeros = new System.Windows.Forms.Button();
+            this.tDato1 = new System.Windows.Forms.TextBox();
+            this.tDato2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbSalida
+            // 
+            this.lbSalida.FormattingEnabled = true;
+            this.lbSalida.Location = new System.Drawing.Point(270, 212);
+            this.lbSalida.Name = "lbSalida";
+            this.lbSalida.Size = new System.Drawing.Size(271, 95);
+            this.lbSalida.TabIndex = 0;
+            // 
+            // bValorarNumeros
+            // 
+            this.bValorarNumeros.Location = new System.Drawing.Point(336, 143);
+            this.bValorarNumeros.Name = "bValorarNumeros";
+            this.bValorarNumeros.Size = new System.Drawing.Size(135, 40);
+            this.bValorarNumeros.TabIndex = 1;
+            this.bValorarNumeros.Text = "VALORAR NÚMEROS";
+            this.bValorarNumeros.UseVisualStyleBackColor = true;
+            this.bValorarNumeros.Click += new System.EventHandler(this.bValorarNumeros_Click);
+            // 
+            // tDato1
+            // 
+            this.tDato1.Location = new System.Drawing.Point(206, 154);
+            this.tDato1.Name = "tDato1";
+            this.tDato1.Size = new System.Drawing.Size(100, 20);
+            this.tDato1.TabIndex = 2;
+            // 
+            // tDato2
+            // 
+            this.tDato2.Location = new System.Drawing.Point(494, 154);
+            this.tDato2.Name = "tDato2";
+            this.tDato2.Size = new System.Drawing.Size(100, 20);
+            this.tDato2.TabIndex = 3;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tDato2);
+            this.Controls.Add(this.tDato1);
+            this.Controls.Add(this.bValorarNumeros);
+            this.Controls.Add(this.lbSalida);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.Text = "Valoración de dos números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSalida;
+        private System.Windows.Forms.Button bValorarNumeros;
+        private System.Windows.Forms.TextBox tDato1;
+        private System.Windows.Forms.TextBox tDato2;
+    }
+}
+
---
+++ DosNumeros/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DosNumeros
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void bValorarNumeros_Click(object sender, EventArgs e)
+        {
+            /* SE VA CAPTAR LO TECLEADO POR EL USUARIO EN LAS 
+             * DOS CAJAS DE TEXTO. COMPROBANDO QUE SOLO TECLEE NÚMEROS */
+
+            bool noError1 = int.TryParse(tDato1.Text, out int valor1);
+            bool noError2 = int.TryParse(tDato2.Text, out int valor2);
+
+            if(noError1 && noError2)
+            {
+                if(valor1 < valor2)
+                {
+                    lbSalida.Items.Add(valor1 + " es MENOR que " + valor2);
+                }
+                else if (valor1 > valor2)
+                {
+                    lbSalida.Items.Add(valor1 + " es MAYOR que " + valor2);
+                }
+                else
+                {
+                    lbSalida.Items.Add(valor1 + " es IGUAL que " + valor2);
+                }
+            }
+            else if(!noError1 && !noError2)
+            {
+                MessageBox.Show("EL PRIMER Y SEGUNDO DATO NO ES UN NÚMERO");
+            }
+            else if(!noError1)
+            {
+                MessageBox.Show("EL PRIMER DATO NO ES UN NÚMERO");
+            }
+            else if(!noError2)
+            {
+                MessageBox.Show("EL SEGUNDO DATO NO ES UN NÚMERO");
+            }
+
+
+
+
+        }
+    }
+}
